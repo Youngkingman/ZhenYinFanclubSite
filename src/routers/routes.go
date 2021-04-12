7ce3527faa636dbef85314bc780954b6c571f0e8
Youngkingman <879375254@qq.com>
2021-04-12 22:00:03 +0800

    sl

--- src/routers/routes.go
+++ src/routers/routes.go
@@ -13,7 +13,7 @@ func SetupRouter() *gin.Engine {
 	// 初始化 Gin 框架默认实例，该实例包含了路由、中间件以及配置信息
 	r := gin.Default()
 	//加载模板路由
-	r.LoadHTMLGlob("view/*")
+	//r.LoadHTMLGlob("view/*")
 	// Ping 测试路由
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
